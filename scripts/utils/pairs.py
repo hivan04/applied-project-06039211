a8325df72cc1c8875200e5b1a0c9d8a907090cbb
Ivan <hungivan04@gmail.com>
2026-04-17 14:25:08 +0100

    17.04
    Environment setup complete

---
+++ scripts/utils/pairs.py
@@ -0,0 +1,9 @@
+# put libraries here
+
+import kalman.py
+
+"""
+Start by creating the simple hedge ratio first and save the results into .xslx or something
+Then form the dynamic model and import the kalman filter from the kalman.py script
+"""
+
